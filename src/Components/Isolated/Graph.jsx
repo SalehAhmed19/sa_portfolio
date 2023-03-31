6690d098a6660b584c2df5f52d99f83eaefadfe3
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-03-31 15:05:09 +0600

    github heatmap showed

--- src/Components/Isolated/Graph.jsx
+++ src/Components/Isolated/Graph.jsx
@@ -1,52 +1,33 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import CalendarHeatmap from "react-calendar-heatmap";
+import GitHubCalendar from "react-github-calendar";
 import "react-calendar-heatmap/dist/styles.css";
 import "../../Styles/Heatmap.css";
 import { Tooltip } from "react-tooltip";
 
 function Graph() {
-  const SecretKey = import.meta.env.VITE_GITHUB_SECRET_TOKEN;
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get(
-        "https://api.github.com/users/SalehAhmed19/repos",
-        {
-          headers: {
-            Authorization: `token ${SecretKey}`,
-          },
-        }
-      );
-      setData(result.data);
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div className="my-20">
+      <Tooltip />
       <h3 className="font-bold text-2xl">GitHub Heatmap</h3>
       <div className="flex w-[225px]">
         <div className="h-[2px] bg-[#007CED] w-[20%] rounded-l-xl"></div>
         <div className="h-[2px] bg-[#353535] w-[80%] rounded-r-xl"></div>
       </div>
       <div className="my-10">
-        <CalendarHeatmap
-          startDate={new Date("2022-01-01")}
+        <GitHubCalendar
+          style={{ margin: "0 auto" }}
+          username="SalehAhmed19"
+          startDate="2021-12-31"
           endDate={new Date()}
-          values={data.map((repo) => ({
-            date: new Date(repo.updated_at),
-            count: repo.stargazers_count,
-            title: `Date: ${new Date(
-              repo.updated_at
-            ).toLocaleDateString()} Count: ${repo.stargazers_count}`,
-          }))}
-          data-tip=""
-          data-for="heatmap-tooltip"
+          tooltip={(data) => `${data.date} - ${data.count} contributions`}
+          onMouseOver={(data, e) => {
+            Tooltip.show(e.target, e);
+          }}
+          onMouseOut={() => {
+            Tooltip.hide();
+          }}
         />
-        <Tooltip id="heatmap-tooltip" />
       </div>
     </div>
   );
